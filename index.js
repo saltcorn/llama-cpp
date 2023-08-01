646e5de06f92f62d162754c88e4e0476a878698f
Tom Nielsen <tanielsen@gmail.com>
2023-08-01 13:57:36 +0100

    pick model

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const Handlebars = require("handlebars");
 const util = require("util");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
+
 const exec = util.promisify(require("child_process").exec);
 
 const configuration_workflow = (req) =>
@@ -22,9 +24,11 @@ const configuration_workflow = (req) =>
               : { name: context.exttable_name }
           );
           //console.log(context);
-          const field_options = table.fields.filter(
-            (f) => f.type?.name === "String"
+          const int_field_options = table.fields.filter(
+            (f) => f.type?.name === "Integer"
           );
+
+          const models = fs.readdirSync(path.join(llama_dir, "models"));
           return new Form({
             fields: [
               {
@@ -33,9 +37,24 @@ const configuration_workflow = (req) =>
                 type: "String",
                 fieldview: "textarea",
                 sublabel: div(
-                  "Use handlebars to access fields. Example: <code>&lt;h1&gt;{{name}}&lt;/h1&gt;</code>. Variables in scope: " +
+                  "Use handlebars to access fields. Example: <code>My name is {{name}}. How are you?</code>. Variables in scope: " +
                     table.fields.map((f) => `<code>${f.name}</code>`).join(", ")
                 ),
+              },
+              {
+                label: "Num. tokens field",
+                name: "ntokens_field",
+                type: "String",
+                attributes: { options: int_field_options.map((f) => f.name) },
+                sublabel:
+                  "Override number of tokens set in instance parameters with value in this field, if chosen",
+              },
+              {
+                label: "Model",
+                name: "model",
+                type: "String",
+                required: true,
+                attributes: { options: models },
               },
             ],
           });
@@ -59,6 +78,15 @@ module.exports = {
       ],
       hyperparameter_fields: ({ table, configuration }) => [
         {
+          name: "ntokens",
+          label: "Num tokesn",
+          type: "Integer",
+          attributes: { min: 1 },
+          required: true,
+          default: 128,
+          sublabel: "Can be overridden by number of tokens field, if set",
+        },
+        {
           name: "temp",
           label: "Temperature",
           type: "Float",
@@ -77,7 +105,7 @@ module.exports = {
       predict: async ({
         id, //instance id
         model: {
-          configuration: { prompt_template },
+          configuration: { prompt_template, ntokens_field, model },
           table_id,
         },
         hyperparameters,
@@ -88,18 +116,24 @@ module.exports = {
         const template = Handlebars.compile(prompt_template || "");
 
         let hyperStr = "";
-        if (hyperparameters.temp) hyperStr += `--temp ${hyperparameters.temp}`;
+        if (hyperparameters.temp) hyperStr += ` --temp ${hyperparameters.temp}`;
         if (hyperparameters.repeat_penalty)
-          hyperStr += `--repeat-penalty ${hyperparameters.repeat_penalty}`;
+          hyperStr += ` --repeat-penalty ${hyperparameters.repeat_penalty}`;
 
         for (const row of rows) {
           const prompt = template(row);
+          let nstr = "";
+          if (ntokens_field && row[ntokens_field])
+            nstr = `-n ${row[ntokens_field]}`;
+          else if (hyperparameters.ntokens)
+            nstr = `-n ${hyperparameters.ntokens}`;
+
           console.log("running llama with prompt: ", prompt);
-          const { stdout } = await exec(
-            `./main -m ./models/llama-2-7b-chat.ggmlv3.q4_K_M.bin -p "${prompt}" -n 16 -t 4 ${hyperStr}`,
+          const { stdout, stderr } = await exec(
+            `./main -m ./models/${model} -p "${prompt}" ${nstr}${hyperStr}`,
             { cwd: llama_dir }
           );
-          console.log("llama result", stdout);
+          console.log("llama result", stderr, stdout);
           results.push({ output: stdout, prompt });
         }
         return results;
@@ -107,10 +141,3 @@ module.exports = {
     },
   },
 };
-
-/* todo
-
-set ntokens
-custom directory
-custom model
-*/
